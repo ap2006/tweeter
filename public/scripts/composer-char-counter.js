2a56dce1182b8f551769115ad804033dafed07ce
Amita <amita.parikh@gmail.com>
2019-03-19 22:02:20 +0000

    Implemented character counter this function, made the counter go down and changed the colour accordingly

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
-  $(".new-tweet textarea").bind("input", (event) => {
-      console.log(event);
+
+var maxLength = 140;
+  $(".new-tweet textarea").on("input", function(event) {
+    var length = $(this).val().length;
+    var charactersRemaining = maxLength - length;
+    var $counter = $('.new-tweet .counter')
+    $counter.text(charactersRemaining);
+    if (length > maxLength) {
+      $counter.addClass("invalid")
+    } else {
+      $counter.removeClass("invalid")
+    }
+      // console.log(this.value.length);
+      // // console.log(event.target.value.length);
     });
 });
